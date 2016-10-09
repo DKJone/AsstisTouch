5f8b5b8da345912a5e72978b67215607357d0142
DKJone <1136165636@qq.com>
2016-10-09 17:15:27 +0800

    烟花效果

---
+++ AsstisTouch3/AsstisTouch3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AsstisTouch3
+//
+//  Created by mac on 16/10/8.
+//  Copyright © 2016年 南京麦伦思. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
